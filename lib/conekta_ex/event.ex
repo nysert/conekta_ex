3e1a596d2daebed9b69789ca3b429d0a67f047ca
luis <ponceluisangel96@gmail.com>
2018-04-29 02:47:40 -0500

    Handles map attributes in ConektaEx.Event.decode/1

--- lib/conekta_ex/event.ex
+++ lib/conekta_ex/event.ex
@@ -18,18 +18,45 @@ defmodule ConektaEx.Event do
 
   ## Examples
 
-      iex> decode(valid_json)
+      iex> decode(valid_bin)
       {:ok, %ConektaEx.Event{}}
 
-      iex> decode(invalid_json)
+      iex> decode(invalid_bin)
       {:error, invalid_json}
 
+      iex> decode(valid_map)
+      {:ok, %ConektaEx.Event{}}
+
+      iex> decode(invalid_map)
+      {:error, invalid_map}
+
   """
-  @spec decode(binary()) :: {:ok, t} | {:error, binary()}
+  @spec decode(binary() | %{required(binary()) => any()}) :: {:ok, t} | {:error, binary() | map()}
   def decode(bin) when is_binary(bin) do
     case Poison.decode(bin, as: %ConektaEx.Event{}) do
       {:error, _} -> {:error, bin}
       {:ok, s} -> {:ok, s}
     end
+  end
+
+  def decode(attr) when is_map(attr) do
+    case to_struct(__MODULE__, attr) do
+      %{id: id} when is_nil(id) ->
+        {:error, attr}
+
+      %{id: id} = event when is_binary(id) ->
+        {:ok, event}
+    end
+  end
+
+  defp to_struct(kind, attrs) do
+    struct = struct(kind)
+
+    Enum.reduce(Map.to_list(struct), struct, fn {k, _}, acc ->
+      case Map.fetch(attrs, Atom.to_string(k)) do
+        {:ok, v} -> %{acc | k => v}
+        :error -> acc
+      end
+    end)
   end
 end
